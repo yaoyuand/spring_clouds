5b5e6c7a4a933964d7bf2a7665e0866ff4a44ab6
yaoyuan <1530858803@qq.com>
2020-11-25 23:43:55 +0800

    订单类

---
+++ cloud-consumer-order80/src/main/java/com/guigu/cloud/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.guigu.cloud.controller;
+
+import com.guigu.cloud.entity.Payment;
+import com.guigu.cloud.entity.ResultData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+@Slf4j
+public class OrderController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static String BASE_PATH="http://localhost:8001";
+
+    @GetMapping("/order/{id}")
+    public ResultData selectById(@PathVariable("id")String id){
+        return restTemplate.getForObject(BASE_PATH+"/payment/"+id,ResultData.class);
+    }
+
+    @GetMapping("/order/save")
+    public ResultData save(Payment payment) {
+        return restTemplate.postForObject(BASE_PATH+"/payment/save",payment,ResultData.class);
+    }
+}
