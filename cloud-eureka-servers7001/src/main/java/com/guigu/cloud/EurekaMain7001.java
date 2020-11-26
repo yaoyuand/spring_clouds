487a9789260c17c13d36e7fcfdf7881056a8847b
yaoyuan <1530858803@qq.com>
2020-11-26 17:11:53 +0800

    服务注册改为eureka集群配置

--- cloud-eureka-servers7001/src/main/java/com/guigu/cloud/EurekaMain7001.java
+++ cloud-eureka-servers7001/src/main/java/com/guigu/cloud/EurekaMain7001.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * eureka服务集群
+ * */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaMainApplication {
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaMainApplication.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
