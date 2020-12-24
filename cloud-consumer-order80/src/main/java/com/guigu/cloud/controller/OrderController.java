e52954b4686ea6806ea8e18b8fb4a45d0f0b7fc7
yaoyuan <1530858803@qq.com>
2020-12-24 15:26:29 +0800

    修改ribbon默认负载均衡策略

--- cloud-consumer-order80/src/main/java/com/guigu/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/guigu/cloud/controller/OrderController.java
@@ -4,11 +4,13 @@ import com.guigu.cloud.entity.Payment;
 import com.guigu.cloud.entity.ResultData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 
 
 @RestController
@@ -17,6 +19,7 @@ public class OrderController {
 
     @Autowired
     private RestTemplate restTemplate;
+
 
 //    private static String BASE_PATH="http://localhost:8001";
     private static String BASE_PATH="http://CLOUD-PAYMENT-SERVICE";//通过服务名称调用
