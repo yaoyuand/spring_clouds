3e45d390bf5c1aa8e0a8394071ca7e8ed55b2283
yaoyuan <1530858803@qq.com>
2020-12-24 16:00:44 +0800

    修改ribbon默认负载均衡策略

--- cloud-consumer-order80/src/main/java/com/guigu/cloud/rule/MyRules.java
+++ cloud-consumer-order80/src/main/java/com/guigu/cloud/rule/MyRules.java
@@ -8,10 +8,13 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * ExcludeComponScan 不被componentScan注解扫描到
+ *
+ * 修改默认方式负载均衡
  * */
 @Configuration
 @ExcludeComponScan
 public class MyRules {
+
     @Bean
     public IRule rules(){
         return new RandomRule();
