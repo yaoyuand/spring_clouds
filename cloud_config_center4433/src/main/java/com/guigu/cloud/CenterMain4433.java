e95634b730cc25428161f58a4738523ab14446eb
yaoyuan <1530858803@qq.com>
2021-01-06 17:46:45 +0800

    添加spring cloud bus配置

--- cloud_config_center4433/src/main/java/com/guigu/cloud/CenterMain4433.java
+++ cloud_config_center4433/src/main/java/com/guigu/cloud/CenterMain4433.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 自动全部刷新
+ * http://localhost:4433/actuator/bus-refresh
+ * 刷新指定服务
+ * http://localhost:4433/actuator/bus-refresh/config-client:4455
+ * */
 @SpringBootApplication
 @EnableConfigServer
 public class CenterMain4433 {
