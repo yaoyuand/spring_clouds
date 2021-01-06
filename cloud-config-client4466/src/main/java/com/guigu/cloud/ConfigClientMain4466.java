62d6823fd24bbcc13c69286de1fa6ab4859171aa
yaoyuan <1530858803@qq.com>
2021-01-06 17:45:29 +0800

    添加spring cloud bus配置

---
+++ cloud-config-client4466/src/main/java/com/guigu/cloud/ConfigClientMain4466.java
@@ -0,0 +1,13 @@
+package com.guigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain4466 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain4466.class,args);
+    }
+}
