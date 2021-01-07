afc9ee828c73f86a2a9e197eb6bb887c29a29205
yaoyuan <1530858803@qq.com>
2021-01-07 17:51:24 +0800

    spring cloud stream-rabbitmq消息接收端

---
+++ cloud-stream-consumer8802/src/main/java/com/guigu/cloud/StreamConsumerMain8802.java
@@ -0,0 +1,11 @@
+package com.guigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamConsumerMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamConsumerMain8802.class,args);
+    }
+}
---
+++ cloud-stream-consumer8802/src/main/java/com/guigu/cloud/controller/ConsumerController.java
@@ -0,0 +1,24 @@
+package com.guigu.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+
+/**
+ * sink为消息接收
+ * StreamListener 进行消息监听类似于rabbitmqListen
+ * */
+@EnableBinding(Sink.class)
+public class ConsumerController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @StreamListener(Sink.INPUT)
+    public void receive(String receive){
+//        String receive=message.getPayload();
+        System.out.println("获取mq传递值为:"+receive+" ,port:"+port);
+    }
+}
